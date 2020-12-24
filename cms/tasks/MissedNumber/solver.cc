9656e0f7eccb22afd85713748014c045ef5867ba
Dinar Karimov <dinarik953@gmail.com>
2020-12-25 00:09:19 +0300

    Improved MissedNumber task, added generator for it

--- cms/tasks/MissedNumber/solver.cc
+++ cms/tasks/MissedNumber/solver.cc
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int64_t n, s=0, temp;
+    uint64_t n, s=0, temp;
     cin >> n;
-    for(int64_t i=0; i<n-1; ++i){
+    for(uint64_t i=0; i<n-1; ++i){
         cin >> temp;
         s+=temp;
     }
